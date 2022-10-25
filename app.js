0f44b39bb2d08baa284057982ef0bd9c0d3b2a20
akansha272 <79163946+akansha272@users.noreply.github.com>
2022-10-25 16:51:18 +0530

    a

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
-app.get('/', fucntion(req,res){
-    res.send('<h1>HELO</h1>');
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
+    res.sendFile(htmlFilePath);
 });
 
 app.get('/restaurants',function(req,res) {
-    
+    const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
+    res.sendFile(htmlFilePath);
+});
+
+app.get('/recommend',function(req,res) {
+    const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
+    res.sendFile(htmlFilePath);
+});
+
+app.get('/about',function(req,res) {
+    const htmlFilePath = path.join(__dirname, 'views', 'about.html');
+    res.sendFile(htmlFilePath);
 });
 
 app.listen(3000);
