34c51ef8c8a48fba7768d091383ec531dc1080fe
akansha272 <79163946+akansha272@users.noreply.github.com>
2022-10-26 15:54:43 +0530

    corrected styles issue

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', function(req,res){
     const htmlFilePath = path.join(__dirname, 'views', 'index.html');
@@ -24,6 +25,10 @@ app.get('/recommend',function(req,res) {
 app.get('/about',function(req,res) {
     const htmlFilePath = path.join(__dirname, 'views', 'about.html');
     res.sendFile(htmlFilePath);
+});
+
+app.post('/recommend', function(req,res) {
+    req.body
 });
 
 app.listen(3000);
