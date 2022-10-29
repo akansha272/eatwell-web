854c270590961fc297698a4ec12529deda1aa8c1
akansha272 <79163946+akansha272@users.noreply.github.com>
2022-10-29 16:04:19 +0530

    404500

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
+const uuid = require('uuid');
 
 const app = express();
 
@@ -26,7 +27,19 @@ app.get('/restaurants',function(req,res) {
 
 app.get('/restaurants/:id', function(req,res) {
     const restaurantId = req.params.id;
-})
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+    for(const restaurant of storedRestaurants){
+        if(restaurant.id === restaurantId){
+            return res.render('restaurant-detail', { restaurant:restaurant})
+        }
+    } 
+
+    res.status(404).render('404');
+});
 
 app.get('/recommend',function(req,res) {
     res.render('recommend');
@@ -34,6 +47,7 @@ app.get('/recommend',function(req,res) {
 
 app.post('/recommend', function(req,res) {
     const restaurant = req.body;
+    restaurant.id = uuid.v4();
     const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
     const fileData = fs.readFileSync(filePath);
@@ -54,6 +68,12 @@ app.get('/about',function(req,res) {
     res.render('about');
 });
 
+app.use(function(req,res){
+    res.status(404).render('404');
+});
 
+app.use(function(error, req, res, next){
+    res.status(500).render('500')
+});
 
 app.listen(3000);
